5fc15a7760ce1b80d52f395c09d33458e4eebcbf
gutovesco <guto.vesco@hotmail.com>
2021-01-13 11:17:29 -0300

    added database connection and created user entity

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,31 @@
+import 'reflect-metadata';
 import express from "express";
+import {createConnection} from 'typeorm';
+import { __prod__ } from './constants';
+import { join } from 'path';
 
 (async () => {
+    try{
+        await createConnection({
+            type: 'postgres',
+            database: 'postgres',
+            entities: [join(__dirname, './entities/*.*')],
+            logging: !__prod__,
+            synchronize: !__prod__,
+            port: 5666,
+            username: 'postgres',
+            password: 'guto12+34'
+        });
+    } catch(err){
+        console.log(err);
+    }
+
     const app = express();
     app.listen(3002, () => {
         app.get('/', (_, res) => {
-            res.send("hello")
-        })
+            res.send("hello");
+        });
         console.log('listening on 3002!');
-    })
+        console.log('dawdwadwda');
+    });
 })();
