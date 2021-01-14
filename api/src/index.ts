e2afe68ea5a45d8109182e7e73aa506171863d0e
gutovesco <guto.vesco@hotmail.com>
2021-01-14 15:32:34 -0300

    added github OAuth to project and changed port to 3006

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
+require("dotenv-safe").config();
 import express from "express";
-import {createConnection} from 'typeorm';
+import { createConnection } from 'typeorm';
 import { __prod__ } from './constants';
 import { join } from 'path';
+import { Strategy as GitHubStrategy } from 'passport-github';
+import passport from 'passport';
 
 (async () => {
-    try{
+    try {
         await createConnection({
             type: 'postgres',
             database: 'postgres',
@@ -16,16 +19,39 @@ import { join } from 'path';
             username: 'postgres',
             password: 'guto12+34'
         });
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
 
     const app = express();
-    app.listen(3002, () => {
+    passport.serializeUser((user: any, done) => {
+        done(null, user.accessToken);
+    });
+    app.use(passport.initialize());
+
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: "http://localhost:3006/auth/github/callback"
+    },
+        (_, __, profile, cb) => {
+            console.log(profile);
+            cb(null, { accessToken: 'd5a76w4d65awd5w', refreshToken: 'dawdkawodjawd7d6' });
+        }
+    ));
+
+    app.get('/auth/github', passport.authenticate('github', { session: false }));
+
+    app.get('/auth/github/callback',
+        passport.authenticate('github', { session: false }),
+        (_, res) => {
+            res.send('successfull login');
+        });
+
+    app.listen(3006, () => {
         app.get('/', (_, res) => {
             res.send("hello");
         });
-        console.log('listening on 3002!');
-        console.log('dawdwadwda');
+        console.log('listening on 3006!');
     });
 })();
