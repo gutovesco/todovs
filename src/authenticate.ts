b5d4317e4a032bf5d74ecb101102e8397957b248
gutovesco <guto.vesco@hotmail.com>
2021-01-18 16:18:05 -0300

    save token in vscode globalState

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,6 +1,31 @@
+import { Request } from 'polka';
 import * as vscode from 'vscode';
 import { apiBaseUrl } from './constants';
+import { TokenManager } from './TokenManager';
+const polka = require('polka');
 
 export const authenticate = () => {
-    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${apiBaseUrl}/auth/github`));
+    const app = polka();
+
+    app.get(`/auth/:token`, async (req: Request, res: any) => {
+        const { token } = req.params;
+        if (!token) {
+            res.end(`<h1>Something went wrong!</h1>`);
+            return;
+        }
+        
+        await TokenManager.setToken(token);
+
+        res.end(`<p>Auth was succesfull. You can close this now</p>`);
+
+        (app as any).server.close();
+    });
+
+    app.listen(54321, (err: Error) => {
+        if (err) {
+            vscode.window.showErrorMessage(err.message);
+        } else {
+            vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`${apiBaseUrl}/auth/github`));
+        }
+    });
 };
--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,30 @@ import * as vscode from 'vscode';
 import { authenticate } from './authenticate';
 import { HelloWorldPanel } from './HelloWorldPanel';
 import { SidebarProvider } from './SidebarProvider';
+import { TokenManager } from './TokenManager';
 
 
 export function activate(context: vscode.ExtensionContext) {
+	TokenManager.globalState = context.globalState;
+	console.log("token value is: ", TokenManager.getToken());
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      "todovs-sidebar",
-      sidebarProvider
-    )
-  );
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"todovs-sidebar",
+			sidebarProvider
+		)
+	);
 
-  const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-  item.text = "$(file-code) Add todo";
-  item.command = 'todovs.addTodo';
-  item.show();
+	const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	item.text = "$(file-code) Add todo";
+	item.command = 'todovs.addTodo';
+	item.show();
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('todovs.helloWorld', () => {
-			HelloWorldPanel.createOrShow(context.extensionUri);
+			vscode.window.showInformationMessage(
+				"token value is: " + TokenManager.getToken()
+			);
 		})
 	);
 
@@ -32,15 +37,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('todovs.addTodo', () => {
-			const {activeTextEditor} = vscode.window;
+			const { activeTextEditor } = vscode.window;
 
-			if(!activeTextEditor){
+			if (!activeTextEditor) {
 				vscode.window.showInformationMessage("No active text editor");
 				return;
 			}
 
 			const text = activeTextEditor.document.getText(activeTextEditor.selection);
-			
+
 			sidebarProvider._view?.webview.postMessage({
 				type: "new-todo",
 				value: text
@@ -64,8 +69,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand("todovs.caioQuestion", async () => {
 			const answer = await vscode.window.showInformationMessage("Do you like bread?", "yes", "yes");
-			
-			if(answer === "sim" || answer === undefined){
+
+			if (answer === "sim" || answer === undefined) {
 				vscode.window.showInformationMessage("Indeed");
 			} else {
 				console.log(answer);
